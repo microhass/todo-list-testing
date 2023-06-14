cea1873a26008b3e73d2b48a0f2b8d1998f57c24
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-14 13:14:25 +0300

    Mocked the localstorage api

---
+++ tests/mocks/localStorage.js
@@ -0,0 +1,15 @@
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+}
+
+export default new LocalStorageMock();
