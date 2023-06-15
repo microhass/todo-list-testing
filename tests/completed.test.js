40166d12e7b8162fde9eac033a7be0d8a425730d
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-15 15:38:00 +0300

    Run function tests and fixed linter errors

--- tests/completed.test.js
+++ tests/completed.test.js
@@ -1,19 +1,15 @@
-
 import {
-    editTaskDescription,
-    updateTaskCompletedStatus,
-    clearCompletedTasks,
-  } from '../src/modules/crud.js';
-  
-  
-  import localStorageMock from '../src/mocks/localStorageMock';
-  
+  updateTask,
+  deleteTasks,
+  markCompleted,
+} from '../src/modules/crud.js';
 
-  global.localStorage = localStorageMock;
-  
-  describe('Task Functions', () => {
-    
-    document.body.innerHTML = `
+import localStorageMock from './mocks/localStorage.js';
+
+global.LocalStorage = localStorageMock;
+
+describe('Task Functions', () => {
+  document.body.innerHTML = `
       <ul class="list">
         <li class="task" id="1">
           <input type="checkbox" name="check" id="check" />
@@ -23,73 +19,49 @@ import {
         </li>
       </ul>
     `;
-  
-    describe('editTaskDescription', () => {
-      test('should update the task description', () => {
-        const taskId = 1;
-        const newDescription = 'Updated Task 1';
-  
-        
-        editTaskDescription(taskId, newDescription);
-  
-        
-        const updatedTasks = JSON.parse(localStorage.getItem('todos'));
-  
-        
-        expect(updatedTasks[0].description).toBe(newDescription);
-      });
-    });
-  
-    describe('updateTaskCompletedStatus', () => {
-      test('should update the task completed status', () => {
-        const taskId = 1;
-        const newCompletedStatus = true;
-  
-        
-        updateTaskCompletedStatus(taskId, newCompletedStatus);
-  
-        
-        const updatedTasks = JSON.parse(localStorage.getItem('todos'));
-  
-        
-        expect(updatedTasks[0].completed).toBe(newCompletedStatus);
-      });
-    });
-  
-    describe('clearCompletedTasks', () => {
-      test('should clear all completed tasks', () => {
-        
-        const tasks = [
-          {
-            index: 1,
-            description: 'Task 1',
-            completed: true,
-          },
-          {
-            index: 2,
-            description: 'Task 2',
-            completed: false,
-          },
-          {
-            index: 3,
-            description: 'Task 3',
-            completed: true,
-          },
-        ];
-  
-    
-        localStorage.setItem('todos', JSON.stringify(tasks));
-  
-        
-        clearCompletedTasks();
-  
-        
-        const updatedTasks = JSON.parse(localStorage.getItem('todos'));
-  
-        
-        const completedTasks = updatedTasks.filter((task) => task.completed);
-        expect(completedTasks).toHaveLength(0);
-      });
+
+  let tasks;
+  beforeEach(() => {
+    tasks = [
+      {
+        index: 1,
+        description: 'Task 1',
+        completed: false,
+      },
+      {
+        index: 2,
+        description: 'Task 2',
+        completed: true,
+      },
+      {
+        index: 3,
+        description: 'Old description',
+        completed: true,
+      },
+    ];
+  });
+
+  describe('editTaskDescription', () => {
+    test('should update the task description', () => {
+      const taskId = 3;
+      const newDescription = 'Updated Task 1';
+      const updatedTasks = updateTask(taskId, newDescription, tasks);
+      expect(updatedTasks[2].description).toBe(newDescription);
     });
   });
-  
+
+  describe('updateTaskCompletedStatus', () => {
+    test('should update the task completed status', () => {
+      const taskId = 1;
+      const updatedTasks = markCompleted(taskId, tasks);
+      expect(updatedTasks[0].completed).toBe(true);
+    });
+  });
+
+  describe('clearCompletedTasks', () => {
+    test('should clear all completed tasks', () => {
+      const unfinishedTasks = deleteTasks(tasks);
+      expect(unfinishedTasks).toHaveLength(1);
+    });
+  });
+});
